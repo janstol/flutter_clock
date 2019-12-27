94e652b91dd9cc4a333706cabb070a990e39fff3
Jan Štol <honzastol@gmail.com>
2019-12-27 22:12:29 +0100

    fix

--- clock_gallery/lib/main.dart
+++ clock_gallery/lib/main.dart
@@ -7,7 +7,6 @@ import 'package:digital_clock/digital_clock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clock_helper/customizer.dart';
 import 'package:flutter_clock_helper/model.dart';
-import 'package:numeric_clock/numeric_clock.dart';
 
 void main() => runApp(ClockCustomizer((model) => App(clockModel: model)));
 
