3f866b248beda030da4d58b32f0b485ddc1bc740
janstol <janstol@users.noreply.github.com>
2020-01-22 17:18:02 +0000

    Automated deployment: Wed Jan 22 17:18:02 UTC 2020 9515d36f51fff4430f4f3fb0e9f7d9f1d7d6548b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const RESOURCES = {
   "/assets/packages/digital_clock/third_party/PressStart2P-Regular.ttf": "2c404fd06cd67770807d242b2d2e5a16",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/AssetManifest.json": "38c983a41c23cd08aebb6c150a9edddc",
-"/assets/LICENSE": "7a62a8f97816bb9f6aa643760acda9a0",
+"/assets/LICENSE": "96efc529953c125ec2c51c0b6c51de74",
 "/assets/FontManifest.json": "cc7081b529eadc01a941a103504d0e7a",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -12,7 +12,7 @@ const RESOURCES = {
 "/digital_clock/third_party/PressStart2P-Regular.ttf": "2c404fd06cd67770807d242b2d2e5a16",
 "/manifest.json": "3c61d67d24f483bf936f7712859c4a1c",
 "/index.html": "8ef33077bd01dd793a70389c6960cb52",
-"/main.dart.js": "aa8cb51f0238967213d7ece7c6a0aefc"
+"/main.dart.js": "fb39b81a4ec0a962ee0f1e0166416e7f"
 };
 
 self.addEventListener('activate', function (event) {
